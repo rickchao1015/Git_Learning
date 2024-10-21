bc470127cca6ec67c7ce67804181c59a0c080c26
rickchao <rickchao@gmail.com>
2024-10-21 14:35:03 +0800

    create a file and push

---
+++ test_python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 10:42:30 2024
+
+@author: user
+"""
+
+print("welcome to the Python world!")
+age = int(input("Please input your age: "))
+#ages = int(age)
+#if int(input("Please input your age: ")) >= 20 : 
+if age >= 20 : 
+    print("You have right to Vote")
+else:
+    print("You have no right to Vote")
